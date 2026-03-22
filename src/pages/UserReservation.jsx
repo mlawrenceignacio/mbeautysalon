3f6e1ee5e2dccc06ce7242574289fdfab8a034db
Lawrence <mlawrenceignacio@gmail.com>
2026-03-22 18:22:49 +0800

    -

--- src/pages/UserReservation.jsx
+++ src/pages/UserReservation.jsx
@@ -17,8 +17,7 @@ import {
   FaInfoCircle,
   FaClipboardCheck,
   FaStar,
-  FaCircleExclamation,
-} from "react-icons/fa6";
+} from "react-icons/fa";
 
 function UserReservation() {
   const [reservations, setReservations] = useState([]);
